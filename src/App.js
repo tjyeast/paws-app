1273900680ffc338e0acf6c3fc3a1980111a6d4c
lorem <lorem@ipsum.com>
2020-06-21 18:49:46 -0400

    Added in react-animations.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import styled, { keyframes } from 'styled-components';
+import { pulse } from 'react-animations';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const pulseAnimation = keyframes`${pulse}`;
+
+const PulsingDiv = styled.div`
+  animation: ${pulseAnimation} 2s infinite;
+  width: 25px;
+  height: 25px;
+  background-color: black;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <PulsingDiv />
+      </div>
+    )
+  }
 }
-
 export default App;
