37408081d81ecb46adbe01e91d9e7e0a2141ed7a
CRHarding <casey.harding@generalassemb.ly>
2020-06-29 19:55:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React, {Component} from 'react';
-import styled, { keyframes } from 'styled-components';
-import { pulse } from 'react-animations';
-
-const pulseAnimation = keyframes`${pulse}`;
-
-const PulsingDiv = styled.div`
-  animation: ${pulseAnimation} 2s infinite;
-  width: 25px;
-  height: 25px;
-  background-color: black;
-`;
 
 class App extends Component {
   render() {
     return (
       <div>
-        <PulsingDiv />
+        <p>Hello, world!</p>
       </div>
     )
   }
